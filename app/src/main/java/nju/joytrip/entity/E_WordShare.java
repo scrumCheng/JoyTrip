da71ab284a0aa71699e288b5a40851e8435b0c70
wwrong1 <1053285647@qq.com>
2019-01-08 14:38:16 +0800

    添加了文字分享

---
+++ app/src/main/java/nju/joytrip/entity/E_WordShare.java
@@ -0,0 +1,23 @@
+package nju.joytrip.entity;
+
+import cn.bmob.v3.BmobObject;
+
+public class E_WordShare extends BmobObject {
+    private String title;
+    private String content;
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
