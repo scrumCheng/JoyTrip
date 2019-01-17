17d2c46eac5b1c98aaa42e68a65b4979d89cfb76
wwrong1 <1053285647@qq.com>
2019-01-17 19:52:53 +0800

    解决图片获取权限问题，并实现图片适应屏幕正方形显示

---
+++ app/src/main/java/nju/joytrip/Util/DynamicGranted.java
@@ -0,0 +1,46 @@
+package nju.joytrip.Util;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+public class DynamicGranted {
+    private Activity mActivity;
+
+    public DynamicGranted(Activity mActivity) {
+        this.mActivity = mActivity;
+    }
+
+    //添加动态权限
+    public void dynamicShare() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            String[] mPermissionList = new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+//                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.READ_LOGS,
+//                    Manifest.permission.READ_PHONE_STATE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.SET_DEBUG_APP,
+                    Manifest.permission.SYSTEM_ALERT_WINDOW,
+                    Manifest.permission.WRITE_APN_SETTINGS,
+                    Manifest.permission.CAMERA};
+            ActivityCompat.requestPermissions(mActivity, mPermissionList, 123);
+        }
+    }
+
+    /**
+     * 检测是否授予相关权限
+     * @return
+     */
+    public boolean permissionGranted(){
+        //如果没有读写文件的权限
+        if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(mActivity, Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+            //PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA )){
+            return false;
+        }
+        return true;
+    }
+}
